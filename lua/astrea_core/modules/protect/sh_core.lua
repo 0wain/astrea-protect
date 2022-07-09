ee563f00936ee3305c46e813c45d02c4b8a110cb
Owain <owain.jones74@gmail.com>
2022-07-09 16:00:23 +0100

    http logging/protection

--- lua/astrea_core/modules/protect/sh_core.lua
+++ lua/astrea_core/modules/protect/sh_core.lua
@@ -13,7 +13,7 @@ AstreaToolbox.Core.AddSetting("net", "protect", {["type"] = "bool", ["clientside
 AstreaToolbox.Core.AddSubSetting("net", "net_limit", "protect", {["type"] = "number", ["clientside"] = false, ["min"] = 0, ["max"] = 200}, 70, "Net Message Threshold", "The amount of Net Messages needed to be received by a user within the buffer to trigger a prevention.")
 AstreaToolbox.Core.AddSubSetting("net", "net_buffer", "protect", {["type"] = "number", ["clientside"] = false, ["min"] = 1, ["max"] = 10}, 2, "Net Message Buffer", "The buffer between Net Message Threshold resets (Seconds).")
 AstreaToolbox.Core.AddSubSetting("net", "net_print", "protect", {["type"] = "bool", ["clientside"] = false}, true, "Net Message Print", "Log incoming net messages in server console.")
-AstreaToolbox.Core.AddSubSetting("net", "net_whitelist", "protect", {["type"] = "list", ["clientside"] = false, {["type"] = "string", ["min"]= 0, ["max"]= 128, ["printname"] = "Net Message", ["case"] = "lower"}}, {}, "Net Message Whitelist", "The net message")
+AstreaToolbox.Core.AddSubSetting("net", "net_whitelist", "protect", {["type"] = "list", ["clientside"] = false, {["type"] = "string", ["min"]= 0, ["max"]= 128, ["printname"] = "Net Message", ["case"] = "lower"}}, {}, "Net Message Whitelist", "A list of Net Messages that are whitelisted from being monitored.")
 AstreaToolbox.Core.AddSubSetting("net", "net_log", "protect", {
     ["type"] = "table", 
     ["columns"] = {
@@ -34,7 +34,7 @@ AstreaToolbox.Core.AddSubSetting("net", "net_log", "protect", {
             ["datatype"] = "INTEGER",
             ["visible"] = true, 
             ["display"] = function(str)
-                return str
+                return os.date('%c', str)
             end,
             ["printname"] = "Date"
         }
@@ -42,6 +42,38 @@ AstreaToolbox.Core.AddSubSetting("net", "net_log", "protect", {
 
     ["descending"] = true 
 }, {}, "Net Logs", "A list of all logged net violations.")
+
+AstreaToolbox.Core.AddSetting("http", "protect", {["type"] = "bool", ["clientside"] = false}, false, "HTTP", "Should HTTP Requests be monitored?")
+AstreaToolbox.Core.AddSubSetting("http", "http_print", "protect", {["type"] = "bool", ["clientside"] = false}, true, "HTTP Print", "Log incoming HTTP requests in server console.")
+AstreaToolbox.Core.AddSubSetting("http", "http_blacklist", "protect", {["type"] = "list", ["clientside"] = false, {["type"] = "string", ["min"]= 0, ["max"]= 500, ["printname"] = "Domain", ["case"] = "lower"}}, {}, "HTTP Blacklist", "A list of domains that are blocked by the HTTP library.")
+AstreaToolbox.Core.AddSubSetting("http", "http_log", "protect", {
+    ["type"] = "table", 
+    ["columns"] = {
+        [1] = {
+            ["name"] = "url",
+            ["datatype"] = "TEXT",
+            ["visible"] = true,
+            ["printname"] = "URL"
+        }, 
+        [2] = {
+            ["name"] = "method",
+            ["datatype"] = "TEXT",
+            ["visible"] = true,
+            ["printname"] = "Method"
+        }, 
+        [3] = {
+            ["name"] = "date",
+            ["datatype"] = "INTEGER",
+            ["visible"] = true, 
+            ["display"] = function(str)
+                return os.date('%c', str)
+            end,
+            ["printname"] = "Date"
+        }
+    },
+
+    ["descending"] = true 
+}, {}, "HTTP Logs", "A list of all logged HTTP requests.")
 
 -- Load any other files
 if SERVER then
--- lua/astrea_core/modules/protect/sv_core.lua
+++ lua/astrea_core/modules/protect/sv_core.lua
@@ -1,7 +1,7 @@
 local lastWipe = {}
 local messageCount = {}
 
-local function logPrint(msg)
+local function logNetPrint(msg)
     if (!AstreaToolbox.Core.GetSetting("net_print")) then return end -- They don't have printing enabled
     print("[Astrea Protect | Net Messages]", msg)
 end
@@ -28,13 +28,13 @@ function net.Incoming(len, client)
 				if (not lastWipe[id64]) or (not messageCount[id64]) then
 					lastWipe[id64] = 0
 					messageCount[id64] = 0
-					logPrint(string.format("New user being registered to the system: %s (%s)", name, id64))
+					logNetPrint(string.format("New user being registered to the system: %s (%s)", name, id64))
 				end
 
 				if lastWipe[id64] < (CurTime() - AstreaToolbox.Core.GetSetting("net_buffer")) then
 					messageCount[id64] = 0
 					lastWipe[id64] = CurTime()
-					logPrint(string.format("Reset: %s's message counter", name))
+					logNetPrint(string.format("Reset: %s's message counter", name))
 				end
 
 				local C = messageCount[id64] or 0
@@ -42,7 +42,7 @@ function net.Incoming(len, client)
 				messageCount[id64] = C + 1
 
 				if (C) and (!(AstreaToolbox.Core.GetSetting("net_limit") == 0)) and (C > AstreaToolbox.Core.GetSetting("net_limit")) then
-					logPrint(string.format("Kicking %s (%s) for suspected net message spamming", name, id64))
+					logNetPrint(string.format("Kicking %s (%s) for suspected net message spamming", name, id64))
 					client:Kick("Suspected Exploiting")
 
 					AstreaToolbox.Core.AddTableRow("net_log", {string.format("%s (%s)", name, id64), strName, os.time()})
@@ -51,13 +51,102 @@ function net.Incoming(len, client)
 					return
 				end
 
-                logPrint(string.format("Message received from %s (%s). The net message was '%s'. Their current count is: %i", name, id64, strName, messageCount[id64]))
+                logNetPrint(string.format("Message received from %s (%s). The net message was '%s'. Their current count is: %i", name, id64, strName, messageCount[id64]))
 			else
-                logPrint(string.format("Message received from Unknown user. The net message was '%s'", strName))
+                logNetPrint(string.format("Message received from Unknown user. The net message was '%s'", strName))
 			end
 		end
 	end
 	
 	len = len - 16
 	func( len, client )
+end
+
+
+
+local function logHTTPPrint(msg)
+    if (!AstreaToolbox.Core.GetSetting("http_print")) then return end -- They don't have printing enabled
+    print("[Astrea Protect | HTTP]", msg)
+end
+
+function http.Fetch( url, onsuccess, onfailure, header )
+	if AstreaToolbox.Core.GetSetting("http") then
+		if (AstreaToolbox.Core.GetSetting("http_blacklist")) then
+			for k, v in pairs(AstreaToolbox.Core.GetSetting("http_blacklist")) do
+				if (string.find(string.lower(url), string.low(v))) then
+					logHTTPPrint(string.format("Request to %s blocked as the domain is in the blacklist", url))
+					return
+				end
+			end
+		end
+
+		logHTTPPrint(string.format("Fetch request made to %s", url))
+		AstreaToolbox.Core.AddTableRow("http_log", {url, "Fetch", os.time()})
+	end
+
+	local request = {
+		url			= url,
+		method		= "get",
+		headers		= header or {},
+
+		success = function( code, body, headers )
+
+			if ( !onsuccess ) then return end
+
+			onsuccess( body, body:len(), headers, code )
+
+		end,
+
+		failed = function( err )
+
+			if ( !onfailure ) then return end
+
+			onfailure( err )
+
+		end
+	}
+
+	HTTP( request )
+end
+
+function http.Post( url, onsuccess, onfailure, header )
+	if AstreaToolbox.Core.GetSetting("http") then
+		if (AstreaToolbox.Core.GetSetting("http_blacklist")) then
+			for k, v in pairs(AstreaToolbox.Core.GetSetting("http_blacklist")) do
+				if (string.find(string.lower(url), string.low(v))) then
+					logHTTPPrint(string.format("Request to %s blocked as the domain is in the blacklist", url))
+					return
+				end
+			end
+		end
+
+		logHTTPPrint(string.format("Post request made to %s", url))
+		AstreaToolbox.Core.AddTableRow("http_log", {url, "Post", os.time()})
+	end
+
+
+	local request = {
+		url			= url,
+		method		= "post",
+		parameters	= params,
+		headers		= header or {},
+
+		success = function( code, body, headers )
+
+			if ( !onsuccess ) then return end
+
+			onsuccess( body, body:len(), headers, code )
+
+		end,
+
+		failed = function( err )
+
+			if ( !onfailure ) then return end
+
+			onfailure( err )
+
+		end
+	}
+
+	HTTP( request )
 end
